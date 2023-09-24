84dd21818580993ff9d3ad761963db27e6fd4449
june <86581908+jxne00@users.noreply.github.com>
2023-09-24 22:36:21 +0800

    add feature hiding

--- app/page.js
+++ app/page.js
@@ -1,4 +1,10 @@
+'use client';
+import { useState } from 'react';
 import dynamic from 'next/dynamic';
+import '../styles/mystyles.css';
+
+import Button from '@mui/material/Button';
+import StartRoundedIcon from '@mui/icons-material/StartRounded';
 
 // dynamically import clock component
 const Clock = dynamic(() => import('../components/clock'), {
@@ -25,19 +31,85 @@ const Timer = dynamic(() => import('../components/timer'), {
 });
 
 export default function Home() {
+  const [featureVisible, setFeatureVisible] = useState({
+    todo: true,
+    timer: true,
+    notes: true,
+  });
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  // set feature visibility
+  const toggleFeature = (feature) => {
+    setFeatureVisible((prevState) => ({
+      ...prevState,
+      [feature]: !prevState[feature],
+    }));
+  };
+
   return (
     <main className='min-h-screen p-4 md:p-8 lg:p-24 flex flex-col md:relative'>
       <div className='my-4 md:absolute md:top-0 md:left-0 md:ml-4 md:mt-4 md:mb-0'>
         <Clock />
-        <Todo />
+        {featureVisible.todo && <Todo />}
       </div>
 
       <div className='my-4 md:absolute md:top-1/4 md:left-1/2 md:transform md:-translate-x-1/2 md:top-1/5'>
-        <Timer />
+        {featureVisible.timer && <Timer />}
       </div>
 
       <div className='my-4 md:absolute md:bottom-0 md:right-0 w-11/12 md:w-2/3 lg:w-1/2 xl:w-1/3'>
-        <Notes />
+        {featureVisible.notes && <Notes />}
+      </div>
+
+      {/* sidebar that can be expended */}
+      <div
+        className={sidebarOpen ? 'sidebar-open' : 'sidebar-closed'}
+        onClick={() => setSidebarOpen(!sidebarOpen)}>
+        {sidebarOpen ? (
+          <div className='p-4 text-white'>
+            <h1 className='text-2xl font-bold'>Settings</h1>
+            <hr className='my-2' />
+            <p className='text-sm text-gray-400'>un-check to hide</p>
+
+            <div onClick={(e) => e.stopPropagation()} className='flex flex-col'>
+              <label className='my-2 flex items-center'>
+                <input
+                  type='checkbox'
+                  className='mr-2'
+                  checked={featureVisible.todo}
+                  onChange={() => toggleFeature('todo')}
+                />
+                Todo
+              </label>
+
+              <label className='my-2 flex items-center'>
+                <input
+                  type='checkbox'
+                  className='mr-2'
+                  checked={featureVisible.timer}
+                  onChange={() => toggleFeature('timer')}
+                />
+                Timer
+              </label>
+
+              <label className='my-2 flex items-center'>
+                <input
+                  type='checkbox'
+                  className='mr-2'
+                  checked={featureVisible.notes}
+                  onChange={() => toggleFeature('notes')}
+                />
+                Notes
+              </label>
+            </div>
+          </div>
+        ) : (
+          <div>
+            <Button variant='contained' className='bg-slate-400 text-white'>
+              <StartRoundedIcon />
+            </Button>
+          </div>
+        )}
       </div>
     </main>
   );
